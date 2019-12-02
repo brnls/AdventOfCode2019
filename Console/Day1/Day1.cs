62343d1eb2742749a81511721202d97d7a039aa6
brnls <brnls1234@gmail.com>
2019-12-01 16:01:26 -0800

    day1

---
+++ Console/Day1/Day1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Console
+{
+    class Day1
+    {
+        public static int PartA()
+        {
+            var lines = File.ReadAllLines("Day1.txt");
+        }
+}
+}
---
+++ Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine(Day1.PartA());
+        }
+    }
+}
